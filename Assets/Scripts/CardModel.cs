311777500d366c0073c4640dc71a0cb66170cb8e
soar1 <soarshizu@gmail.com>
2020-04-07 21:45:50 +0900

    [fix]CardModel.cs CardDisplay.cs CardEntity.cs Card CardPrefab

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel {
+    public string name;
+    public int hp;
+    public int at;
+    public int cost;
+    public Sprite icon;
+    public Sprite backgroundPanel;
+
+    public CardModel(int cardId) {
+
+        CardEntity cardEntity = Resources.Load<CardEntity>("CardList/Card" + cardId);
+        backgroundPanel = cardEntity.backgroundPanel;
+        name = cardEntity.name;
+        hp = cardEntity.hp;
+        at = cardEntity.at;
+        cost = cardEntity.cost;
+        icon = cardEntity.icon;
+    }
+}
