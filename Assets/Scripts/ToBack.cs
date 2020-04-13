b0b09d6c092dd1dc5383d31d6911fa6c0632b466
unknown <akito030303@gmail.com>
2020-04-14 02:11:24 +0900

    [add] SceneTransition_script

---
+++ Assets/Scripts/ToBack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToBack : MonoBehaviour {
+    public GameObject Panel;
+    private bool IsOn = true;
+
+
+    public void DeletePanel() {
+        if (IsOn) {
+            Panel.SetActive(false);
+            IsOn = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/ToSelectDeckPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToSelectDeckPanel : MonoBehaviour
+{
+    public GameObject Panel;
+    private bool IsOn;
+
+    public void start() {
+        Panel.SetActive(false);
+        IsOn = false;
+    }
+
+    public void PressDeck() {
+        if (!IsOn) {
+            Panel.SetActive(true);
+            
+        }
+    }
+       public void DeletePanel() {
+        if (!IsOn) {
+            Panel.SetActive(false);
+            
+        }
+    }
+}
