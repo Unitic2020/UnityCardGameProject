140018cf55fa47c523022773b29c25fad4159858
b1019116 <b1019116@fun.ac.jp>
2020-05-15 20:29:42 +0900

    ドラッグアンドドロップ処理（リフレッシュ後）

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+
+public class GameManager : MonoBehaviour
+{
 
     [SerializeField] public CardDisplay cardPrefab;
-    [SerializeField] Transform playerHand;
+    public Transform playerHand;
     [SerializeField] Transform enemyHand;
     [SerializeField] GameObject yourTurnPanel;
     [SerializeField] GameObject enemyTurnPanel;
@@ -15,22 +17,35 @@ public class GameManager : MonoBehaviour {
     [SerializeField] Text displayEnemyHp;
     [SerializeField] Text displayPlayerManaCost;
     [SerializeField] Text displayEnemyManaCost;
-    [SerializeField] Text displayNumberOfPlayerHandCard;
+    public Text displayNumberOfPlayerHandCard;
     [SerializeField] Text displayNumberOfPlayerCardInGraveyard;
     [SerializeField] Text displayNumberOfEnemyHandCard;
     [SerializeField] Text displayNumberOfEnemyCardInGraveyard;
 
-    CardDisplay[] playerHandCardList;
+    public CardDisplay[] playerHandCardList;
     CardDisplay[] enemyHandCardList;
 
     public static GameManager instance;
     public bool turn; //true = player, false = enemy 
-    public List<int> playerSampleDeck = new List<int>() { 1, 1, 1, 1, 1, 1, 1 ,1,1,1,1,1,1,1,1,1,1};
-    public List<int> enemySampleDeck = new List<int>() { 1, 1, 1, 1, 1, 1, 1 ,1,1,1,1,1,1,1,1,1,1};
+    public List<int> playerSampleDeck = new List<int>() { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+    public List<int> enemySampleDeck = new List<int>() { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
     int defaultPlayerManaCost = 0;
     int defaultEnemyManaCost = 0;
-    void Start() {
+
+    // 他クラスでもGameManagerのオブジェクトを参照できるように、GameManagerのオブジェクトを、staticで宣言。
+    public static GameManager gameManagerObject;
+
+    void Awake()
+    {
+        if (gameManagerObject == null)
+        {
+            gameManagerObject = this;
+        }
+    }
+
+    void Start()
+    {
 
         // プレイヤーと敵のそれぞれの手札にあるカードの枚数を取得する
         playerHandCardList = playerHand.GetComponentsInChildren<CardDisplay>();
@@ -46,26 +61,33 @@ public class GameManager : MonoBehaviour {
         displayNumberOfPlayerCardInGraveyard.text = "x" + 0.ToString();
         displayNumberOfEnemyCardInGraveyard.text = "x" + 0.ToString();
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++)
+        {
             GiveOutCard(playerSampleDeck, playerHand);
             GiveOutCard(enemySampleDeck, enemyHand);
         }
 
         turn = true;
-        
-        if (turn) {
+
+        if (turn)
+        {
             defaultPlayerManaCost += 1;
             displayPlayerManaCost.text = defaultPlayerManaCost.ToString();
             StartCoroutine(PlayerTurn());
-        } else {
+        }
+        else
+        {
             defaultEnemyManaCost += 1;
             displayEnemyManaCost.text = defaultEnemyManaCost.ToString();
             StartCoroutine(EnemyTurn());
         }
     }
 
+
+
     /*プレイヤーのターンだよ*/
-    IEnumerator PlayerTurn() {
+    IEnumerator PlayerTurn()
+    {
 
         Debug.Log("プレイヤーターン");
         yourTurnPanel.SetActive(true);
@@ -76,7 +98,8 @@ public class GameManager : MonoBehaviour {
     }
 
     /*敵のターンだよ*/
-    IEnumerator EnemyTurn() {
+    IEnumerator EnemyTurn()
+    {
 
         Debug.Log("エネミーターン");
         enemyTurnPanel.SetActive(true);
@@ -87,37 +110,52 @@ public class GameManager : MonoBehaviour {
     }
 
     /*ターンごとに時間制限設けるよ*/
-    IEnumerator TimeSetting() {
-        
+    IEnumerator TimeSetting()
+    {
+
         int time = 5;
         displayTimeCount.text = time.ToString();
 
-        while(time >= 0) {
+        while (time >= 0)
+        {
             yield return new WaitForSeconds(1);
             time -= 1;
             displayTimeCount.text = time.ToString();
         }
-      
+
         SwitchTurn(turn);
     }
 
-    public void SwitchTurn(bool isTurn) {
+    public void SwitchTurn(bool isTurn)
+    {
 
         turn = !isTurn;
-        if (turn) {
+        if (turn)
+        {
             StartCoroutine(PlayerTurn());
-        } else {
+        }
+        else
+        {
             StartCoroutine(EnemyTurn());
         }
         IncreaseManaCost(turn);
-    
+
     }
 
     /*カード配るよ*/
-    public void GiveOutCard(List<int> deck, Transform hand) {
-        int cardId = deck[0]; 
+    public void GiveOutCard(List<int> deck, Transform hand)
+    {
+        int cardId = deck[0];
         CardDisplay card = Instantiate(cardPrefab, hand, false);
         card.Initialize(cardId);
+        if (hand == playerHand)
+        {
+            card.playerCard = true;
+        }
+        else
+        {
+            card.playerCard = false;
+        }
         deck.RemoveAt(0);
 
         playerHandCardList = playerHand.GetComponentsInChildren<CardDisplay>();
@@ -125,12 +163,17 @@ public class GameManager : MonoBehaviour {
 
         displayNumberOfPlayerHandCard.text = "x" + playerHandCardList.Length.ToString();
         displayNumberOfEnemyHandCard.text = "x" + enemyHandCardList.Length.ToString();
+
     }
 
-    public void IncreaseManaCost(bool isPlayerTurn){
-        if (isPlayerTurn){
+    public void IncreaseManaCost(bool isPlayerTurn)
+    {
+        if (isPlayerTurn)
+        {
             defaultPlayerManaCost++;
-        }else{
+        }
+        else
+        {
             defaultEnemyManaCost++;
         }
 
