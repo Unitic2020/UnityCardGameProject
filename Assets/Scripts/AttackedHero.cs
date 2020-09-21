d7f18f26683ae5ecc602d7a2e87ae03c13d2c82a
b1019116 <b1019116@fun.ac.jp>
2020-09-21 14:46:51 +0900

    ヒーローに対する攻撃の処理

---
+++ Assets/Scripts/AttackedHero.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AttackedHero : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("AttackedHero called");
+        CardDisplay attacker = eventData.pointerDrag.GetComponent<CardDisplay>();
+        if (attacker.canAttack){
+            GameManager.gameManagerObject.AttackToHero(attacker, true);
+        }
+    }
+}
+
