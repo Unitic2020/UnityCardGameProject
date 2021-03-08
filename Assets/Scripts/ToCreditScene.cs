f36b6bfea605fa33028bc91994d0bf23c248d581
soar1 <soarshizu@gmail.com>
2021-03-08 16:34:06 +0900

    [Create]Creditシーン、Menuシーンとの相互遷移

---
+++ Assets/Scripts/ToCreditScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToCreditScene:MonoBehaviour {
+    private bool IsPressed = false;
+
+    public void PressCreditButton() {
+
+        if(!IsPressed) {
+            SceneManager.LoadScene("Credit");
+            IsPressed = true;
+        }
+
+    }
+}
